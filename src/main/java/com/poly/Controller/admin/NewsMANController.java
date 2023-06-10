e2ac288a5f6289fbe2463702cdb3535e962c27b9
PiKayQi Nguyễn <101205004+PKQNguyen@users.noreply.github.com>
2023-06-10 08:15:16 +0700

    testnewMans

--- src/main/java/com/poly/Controller/admin/NewsMANController.java
+++ src/main/java/com/poly/Controller/admin/NewsMANController.java
@@ -1,13 +1,32 @@
 package com.poly.Controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.poly.DAO.NewsDAO;
+import com.poly.Entities.News;
 
 @Controller
 public class NewsMANController {
 	
+	@Autowired
+	NewsDAO dao;
+	
 	@GetMapping("/admin/news")
 	public String view() {
+		return "admin/news";
+	}
+	
+	@GetMapping("/admin/news?delete={id}")
+	public String view(
+			@PathVariable("id") Integer ID
+			) {
+		News entity = dao.getById(ID);
+		entity.setIs_active(0);
+		
+		dao.saveAndFlush(entity);
 		return "admin/news";
 	}
 }
--- src/main/java/com/poly/DAO/NewsDAO.java
+++ src/main/java/com/poly/DAO/NewsDAO.java
@@ -1,10 +1,15 @@
 package com.poly.DAO;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.poly.Entities.News;
+import com.poly.entity.Product;
 
 public interface NewsDAO extends JpaRepository<News, Integer>  {
-	
+	@Query("SELECT p FROM News p WHERE p.price BETWEEN ?1 AND ?2")
+	List<News> findByPrice(Double min, Double max);
 	
 }
