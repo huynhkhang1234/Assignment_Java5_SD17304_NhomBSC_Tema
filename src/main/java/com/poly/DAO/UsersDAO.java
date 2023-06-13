e21f216d09bec09a00b1cd7c70a6b92bbd930b9f
khang <huynhkhanglove12345@gmail.com>
2023-06-13 17:15:28 +0700

    ac

--- src/main/java/com/poly/DAO/UsersDAO.java
+++ src/main/java/com/poly/DAO/UsersDAO.java
@@ -5,18 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import com.poly.Entities.News;
 import com.poly.Entities.Users;
 
 public interface UsersDAO extends JpaRepository<Users, Integer> {
 	@Query("SELECT u FROM Users u WHERE u.email = :email")
 	Users findByEmail(@Param("email") String username);
 	
-	@Query("SELECT u FROM Users u WHERE u.email = :email")
-	Users findByEmail(@Param("email") String username);
-	
+
 	public Users findByEmailEquals(String email);
 
 	
